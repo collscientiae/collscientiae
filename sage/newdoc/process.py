ab33078b2f85a0d598cdec513f8bce4a3600e17d
Harald Schilly <harald.schilly@gmail.com>
2014-09-23 18:26:25 +0200

    knowl support via knowl::id|title: with ending colon sign

--- sage/newdoc/process.py
+++ sage/newdoc/process.py
@@ -18,6 +18,15 @@ class HashTagPattern(markdown.inlinepatterns.Pattern):
         el.text = '#' + m.group(2)
         return el
 
+class KnowlTagPatternWithTitle(markdown.inlinepatterns.Pattern):
+    def handleMatch(self, m):
+        tokens = m.group(2).split("|")
+        kid = tokens[0].strip()
+        if len(tokens) > 1:
+            t = ''.join(tokens[1:])
+            return "{{ KNOWL('%s', title='%s') }}" % (kid, t.strip())
+        return "{{ KNOWL('%s') }}" % kid
+
 
 class Processor(object):
 
@@ -31,7 +40,7 @@ class Processor(object):
     """
 
     def __init__(self):
-        self.md = markdown.Markdown(
+        self.md = md = markdown.Markdown(
             extensions=['markdown.extensions.toc',
                         'markdown.extensions.extra',
                         'markdown.extensions.sane_lists',
@@ -39,16 +48,21 @@ class Processor(object):
                         'markdown.extensions.codehilite'])
 
         # Prevent $..$, $$..$$, \(..\), \[..\] blocks from being processed by Markdown
-        self.md.inlinePatterns.add('mathjax$', IgnorePattern(r'(?<![\\\$])(\$[^\$].*?\$)'), '<escape')
-        self.md.inlinePatterns.add('mathjax$$', IgnorePattern(r'(?<![\\])(\$\$.+?\$\$)'), '<escape')
-        self.md.inlinePatterns.add('mathjax\\(', IgnorePattern(r'(\\\(.+?\\\))'), '<escape')
-        self.md.inlinePatterns.add('mathjax\\[', IgnorePattern(r'(\\\[.+?\\\])'), '<escape')
+        md.inlinePatterns.add('mathjax$', IgnorePattern(r'(?<![\\\$])(\$[^\$].*?\$)'), '<escape')
+        md.inlinePatterns.add('mathjax$$', IgnorePattern(r'(?<![\\])(\$\$.+?\$\$)'), '<escape')
+        md.inlinePatterns.add('mathjax\\(', IgnorePattern(r'(\\\(.+?\\\))'), '<escape')
+        md.inlinePatterns.add('mathjax\\[', IgnorePattern(r'(\\\[.+?\\\])'), '<escape')
         # double `` backtick `` for ASCIIMath -- hope this doesn't confuse with <code> single backticks
-        self.md.inlinePatterns.add('mathjax``', IgnorePattern(r'(?<![\\`])(``.+?``)'), '<escape')
+        md.inlinePatterns.add('mathjax``', IgnorePattern(r'(?<![\\`])(``.+?``)'), '<escape')
 
         # Tell markdown to turn hashtags into search urls
         hashtag_keywords_rex = r'#([a-zA-Z][a-zA-Z0-9-_]{1,})\b'
         self.md.inlinePatterns.add('hashtag', HashTagPattern(hashtag_keywords_rex), '<escape')
+
+        # Tells markdown to process "wikistyle" knowls with optional title
+        # should cover {{ KID }} and {{ KID | title }}
+        knowltagtitle_regex = r'knowl::([^:]+):'
+        md.inlinePatterns.add('knowltagtitle', KnowlTagPatternWithTitle(knowltagtitle_regex), '<escape')
 
     def process(self, content, target="html"):
         return getattr(self, "process_%s" % target)(content)
