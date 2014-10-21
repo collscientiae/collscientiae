162727bef64b01950231a2131c45b28343ddabce
Harald Schilly <harald.schilly@gmail.com>
2014-10-21 23:30:33 +0200

    need to untangle knowl and link handlers: knowls should be able to handle more complex sources like includes, but at the same time specify custom titles like includes. DRY: introduce a common superclass for all three of them

--- collscientiae/process.py
+++ collscientiae/process.py
@@ -74,20 +74,17 @@ class IncludePattern(markdown.inlinepatterns.Pattern):
             div.set("limit", limit)
         return div
 
-
-class KnowlAndLinkPattern(markdown.inlinepatterns.Pattern):
+class LinkPattern(markdown.inlinepatterns.Pattern):
 
     def __init__(self, pattern, cp):
         self.cp = cp
-        super(KnowlAndLinkPattern, self).__init__(pattern)
+        super(LinkPattern, self).__init__(pattern)
 
     def handleMatch(self, m):
         from markdown.util import etree
         from .models import namespace_pattern
 
-        type = m.group(2)
-        assert type in ["link", "knowl"]
-        tokens = m.group(3).split("|")
+        tokens = m.group(2).split("|")
         raw_id = tokens[0].strip()
         kidsplit = raw_id.split("/")
         assert document_id_pattern.match(kidsplit[-1]), "Document ID '%s' invalid" % kidsplit[-1]
@@ -101,13 +98,44 @@ class KnowlAndLinkPattern(markdown.inlinepatterns.Pattern):
         assert namespace_pattern.match(target_ns)
         link = target_ns + "/" + doc_id
         a = etree.Element("a")
-        if type == "link":
-            self.cp.db.register_link(target_ns, doc_id, self.cp.document)
-            a.set("href", "../%s.html" % link)
 
-        elif type == "knowl":
-            self.cp.db.register_knowl(target_ns, doc_id, self.cp.document)
-            a.set("knowl", link)
+        self.cp.db.register_link(target_ns, doc_id, self.cp.document)
+        a.set("href", "../%s.html" % link)
+
+        if len(tokens) > 1:
+            t = ''.join(tokens[1:])
+            a.text = t.strip()
+        else:
+            a.text = kidsplit[-1]
+        return a
+
+class KnowlPattern(markdown.inlinepatterns.Pattern):
+
+    def __init__(self, pattern, cp):
+        self.cp = cp
+        super(KnowlPattern, self).__init__(pattern)
+
+    def handleMatch(self, m):
+        from markdown.util import etree
+        from .models import namespace_pattern
+
+        tokens = m.group(2).split("|")
+        raw_id = tokens[0].strip()
+        kidsplit = raw_id.split("/")
+        assert document_id_pattern.match(kidsplit[-1]), "Document ID '%s' invalid" % kidsplit[-1]
+        assert 1 <= len(kidsplit) <= 2
+        if len(kidsplit) == 2:
+            target_ns, doc_id = kidsplit
+        else:
+            target_ns = self.cp.document.namespace
+            doc_id = kidsplit[-1]
+        target_ns = self.cp.cs.remap_module(self.cp.document.namespace, target_ns)
+        assert namespace_pattern.match(target_ns)
+        link = target_ns + "/" + doc_id
+        a = etree.Element("a")
+
+        self.cp.db.register_knowl(target_ns, doc_id, self.cp.document)
+        a.set("knowl", link)
 
         if len(tokens) > 1:
             t = ''.join(tokens[1:])
@@ -255,10 +283,16 @@ class ContentProcessor(object):
                               HashTagPattern(hashtag_keywords_rex, self),
                               '<escape')
 
-        # Tells markdown to process "wikistyle" knowls with optional title
-        linkandknowl_regex = r'(link|knowl)\[([^\]]+)\]'
-        md.inlinePatterns.add('linkknowltag',
-                              KnowlAndLinkPattern(linkandknowl_regex, self),
+        # Tells markdown to process "wikistyle" links with optional title
+        link_regex = r'link\[([^\]]+)\]'
+        md.inlinePatterns.add('linktag',
+                              LinkPattern(link_regex, self),
+                              '<escape')
+
+        # Tells markdown to process "wikistyle" knowosl with optional title
+        knowl_regex = r'knowl\[([^\]]+)\]'
+        md.inlinePatterns.add('knowltag',
+                              KnowlPattern(knowl_regex, self),
                               '<escape')
 
         include_pattern = r'include\[([^\]]+)\]'
